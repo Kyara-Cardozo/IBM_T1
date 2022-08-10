44d62bb52b870a0c0b9f27cb2498cb7cff9b61c5
kyara cardozo <kyarakya13@gmail.com>
2022-08-09 21:00:32 -0300

    commit

---
+++ Tabuada/src/result.java
@@ -0,0 +1,18 @@
+
+public class result {
+
+	public static void main(String[] args) { 
+		  
+		  int result; 
+		   
+		  for (int x = 1; x <= 10; x++) { 
+		   System.out.println("TABUADA DO NÚMERO: " + x); 
+		   for (int y = 1; y <= 10; y++) { 
+			   result = x * y; 
+		    System.out.println(x + " * " + y + " = " + result); 
+		   } 
+		   System.out.println("..........................."); 
+		  } 
+		 } 
+		 
+		} 
