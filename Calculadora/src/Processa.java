af15d3d9d9e60f34cddd53be5a6d9a85a64ae438
kyara cardozo <kyarakya13@gmail.com>
2022-08-09 21:11:08 -0300

    commit

---
+++ Calculadora/src/Processa.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Processa {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Informe o valor!");
+		Scanner teclado = new  Scanner(System.in);
+		int valor = teclado.nextInt();
+		
+		System.out.println("O vlaor informado foi:"+valor);
+		
+		
+
+	}
+	
+
+}
