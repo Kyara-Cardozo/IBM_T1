ce8e59ad363f158f49c536be86b8ea08bf854e5f
kyara cardozo <kyarakya13@gmail.com>
2022-08-09 21:09:42 -0300

    commit

---
+++ Desenvolvimento/src/Mensagem.java
@@ -0,0 +1,31 @@
+
+public class Mensagem {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.print("Esse exemplo é sem o println");
+		System.out.print("Como ficou?");
+		
+		System.out.println("Esse exemplo é com println");
+		System.out.println("Como ficou?");
+
+		
+		// Criando variáveis
+		String nome="Aluna IBM";
+		int idade =23;
+		System.out.println(nome+" com "+idade+" anos, vamos fazer rum suuuuuper desenvolviemnto!");
+		
+		// Sinais operacionais
+		int num1=52;
+		int num2=30;
+		int resultado = num1-num2;
+		
+		System.out.println("O reusltado da soma do número "+num1+" com o número "+num2+" é igual a: "+num1+num2);
+		System.out.println("O reusltado da multiplicação do número "+num1+" com o número "+num2+" é igual a: "+num1*num2);
+		System.out.println("O reusltado da divisão do número "+num1+" com o número "+num2+" é igual a: "+num1/num2);
+		System.out.println("O reusltado da subtração do número "+num1+" com o número "+num2+" é igual a: "+resultado);
+		
+	}
+
+}
