db040a023468d2460368740b1b712753b2d22913
kyara cardozo <kyarakya13@gmail.com>
2022-08-09 21:05:57 -0300

    commit

---
+++ LacoRepeticao/src/ProcessaTabuada.java
@@ -0,0 +1,14 @@
+
+public class ProcessaTabuada {
+
+	public static void main(String[] args) {
+		// criando laco
+		for(int cont=0; cont<=10; cont++) {;
+		System.out.println(cont);
+
+		
+		}
+	}
+}
+
+
